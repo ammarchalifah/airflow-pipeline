13a3cdb46442ef75595177453387380196c2e530
Ammar Chalifah <ammar.chalifah@gmail.com>
2021-08-27 12:17:27 +0700

    mysql connection

---
+++ dags/helpers/mysql_connection.py
@@ -0,0 +1,23 @@
+import logging
+
+from mysql.connector import MySQLConnection, Error
+
+def query_mysql(host, post, user, password, database, query):
+    try:
+        conn = MySQLConnection(host=host, port=port, user=user, password=password, database=database)
+        cursor = conn.cursor()
+        cursor.execute(query)
+
+        logging.info('Query execution done with {} rows'.format(cursor.rowcount))
+
+        rows = cursor.fetchall()
+        
+        for row in rows:
+            logging.info(str(row))
+
+    except Error as e:
+        print(e)
+    
+    finally:
+        cursor.close()
+        conn.close()
